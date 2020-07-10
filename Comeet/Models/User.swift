9b73475bb2dc70b7d9d8b53c2fff54fe09ace9a1
mpross512 <mpross512@gmail.com>
2020-07-10 16:38:04 -0500

    Added Firebase Pod

---
+++ Comeet/Models/User.swift
@@ -0,0 +1,18 @@
+//
+//  User.swift
+//  Comeet
+//
+//  Created by Michael Ross on 7/10/20.
+//  Copyright © 2020 Ice Cream Loaf. All rights reserved.
+//
+
+import Foundation
+
+struct User {
+    
+    let graduationYear: Int
+    let name: String
+    let age: Int
+    let Major: String
+    
+}
---
+++ Comeet/Views/MainView.swift
@@ -0,0 +1,21 @@
+//
+//  MainView.swift
+//  Comeet
+//
+//  Created by Michael Ross on 7/10/20.
+//  Copyright © 2020 Ice Cream Loaf. All rights reserved.
+//
+
+import SwiftUI
+
+struct MainView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+struct MainView_Previews: PreviewProvider {
+    static var previews: some View {
+        MainView()
+    }
+}
