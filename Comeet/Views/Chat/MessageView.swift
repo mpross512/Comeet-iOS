41ae44dd35ddb946796a2bcf9af3f8f7b0d5752a
Isabelle Villegas <izzy57467@gmail.com>
2023-10-30 17:37:10 -0500

    Updated packages

--- Comeet/Views/Chat/MessageView.swift
+++ Comeet/Views/Chat/MessageView.swift
@@ -7,16 +7,36 @@
 //
 
 import SwiftUI
+import FirebaseFunctions
 
 struct MessageView: View {
     
     @State var user: User
+    lazy var functions = Functions.functions()
     
     var body: some View {
 
             VStack {
                 ScrollView {
                     Text("You and \(user.name["first"] ?? "") both like each other!").italic().font(.caption).foregroundColor(.gray)
+                    Button {
+                        functions.httpsCallable("addMessage").call(["text": "this is a test"]) { result, error in
+                          if let error = error as NSError? {
+                            if error.domain == FunctionsErrorDomain {
+                              let code = FunctionsErrorCode(rawValue: error.code)
+                              let message = error.localizedDescription
+                              let details = error.userInfo[FunctionsErrorDetailsKey]
+                            }
+                            // ...
+                          }
+                          if let data = result?.data as? [String: Any], let text = data["text"] as? String {
+                            self.resultField.text = text
+                          }
+                        }
+                    } label: {
+                        Text("Test function")
+                    }
+
                 }
                 
                 
